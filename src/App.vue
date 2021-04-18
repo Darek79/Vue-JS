3f0c0362aad61956bf38236706adff2384383da2
Darek79 <darius.kloda@gmail.com>
2021-04-18 09:47:22 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>hello {{ name }}</h2>
+
+  <button @click="clickUp">up</button>
+  <button @click="clickDown">down</button>
+  <p>my count: {{ counter }}</p>
+  <ul>
+    <li v-for="(nr, i) in arr" :key="nr">
+      {{ nr }} - {{ i + 5 }}
+    </li>
+  </ul>
+  <input :value="txt" @input="write($event)" />
+  <p>{{ txt }}</p>
+  <input v-model="msg" placeholder="edit-me" />
+  <p>{{ msg }}</p>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      name: "Darek",
+      counter: 0,
+      arr: [1, 2, 3, 4, 5],
+      txt: "",
+      msg: "",
+    };
+  },
+  methods: {
+    clickUp() {
+      this.counter = this.counter + 1;
+    },
+    clickDown() {
+      this.counter = this.counter - 1;
+    },
+    write(event) {
+      console.log(event.target);
+      this.txt = event.target.value;
+    },
+  },
+  beforeCreate() {
+    console.log("1");
+  },
+  created() {
+    console.log("2");
+  },
+  beforeMount() {
+    console.log("3");
+  },
+  mounted() {
+    this.counter = 2;
+  },
+  beforeUnmount() {
+    console.log("4");
+  },
+  unmounted() {
+    console.log("5");
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
